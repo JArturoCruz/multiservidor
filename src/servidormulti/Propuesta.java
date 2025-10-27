7a3bcea9d271566e13f2e72b1c5f6f6134161fe2
JArturoCruz <jorge_beltran_cruz@hotmail.com>
2025-10-26 21:20:29 -0700

    agregue la nueva Clase propuesta

--- src/servidormulti/Propuesta.java
+++ src/servidormulti/Propuesta.java
@@ -1,4 +1,19 @@
 package servidormulti;
 
 public class Propuesta {
+    private final UnCliente proponente;
+    private final UnCliente invitado;
+
+    public Propuesta(UnCliente proponente, UnCliente invitado) {
+        this.proponente = proponente;
+        this.invitado = invitado;
+    }
+
+    public UnCliente getProponente() {
+        return proponente;
+    }
+
+    public UnCliente getInvitado() {
+        return invitado;
+    }
 }
